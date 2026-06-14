ed582a9d0a26a653193f10b10e9751902c82e37d
Lopepi <lopepi@outlook.fr>
2026-06-14 13:10:32 +0200

    Created register form

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import RegisteForm from "./RegisterForm";
 
 function Register() {
     return (
         <>
             <Navbar firstButtonLabel="Se Connecter" secondButtonLabel="Accueil" firstButtonLink="login" secondButtonLink="/"></Navbar>
+            <RegisteForm></RegisteForm>
         </>
     );
 }
---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,32 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type FormFields = {
+    email: string;
+    password: string;
+    userName: string;
+}
+
+function RegisteForm() {
+
+    const { register, handleSubmit, formState: { errors} } = useForm<FormFields>();
+
+    const onSubmit: SubmitHandler<FormFields> = (data) => {
+        console.log(data);
+    }
+
+    return(
+        <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("email", {required: "Email is required", pattern: {value: /^[a-z0-9._%+-]+@[a-z0-9._%+-]+\.[a-z]{2,4}$/i, message: "Email must be valid"}})} type="text" placeholder="Email"/>
+            {errors.email && <div>{errors.email.message}</div>}
+            <input {...register("password", {required: "Password is required", minLength: {value: 8, message: "Password must have at least 8 characters"}})} type="text" placeholder="Password"/>
+            {errors.password && <div>{errors.password.message}</div>}
+            <input {...register("userName", {required: "Username is required", minLength: {value: 2, message: "Username must have at least 2 characters"}, maxLength: {value: 42, message: "Username must have less than 43 characters"}})} type="text" placeholder="Username"/>
+            {errors.userName && <div>{errors.userName.message}</div>}
+            <button type="submit">Submit</button>
+        </form>
+        </>
+    );
+}
+
+export default RegisteForm;
